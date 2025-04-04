4b0ceaad324f3ca5dc5f55e6d675905240f6594e
AdmiralCrow <67215398+AdmiralCrow@users.noreply.github.com>
2025-04-03 21:32:22 -0400

    Update memory_manager.h

--- nachos-3.4/code/userprog/memory_manager.h
+++ nachos-3.4/code/userprog/memory_manager.h
@@ -1,23 +1,27 @@
-#ifndef MEMORY_H
-#define MEMORY_H
+#ifndef MEMORY_MANAGER_H
+#define MEMORY_MANAGER_H
 
 #include "bitmap.h"
+#include "synch.h"
 
 class MemoryManager {
+public:
+    // Constructor: initializes the bitmap with the total number of physical pages.
+    MemoryManager(int numTotalPages);
+    
+    // Destructor: cleans up the allocated bitmap and lock.
+    ~MemoryManager();
+    
+    // Allocates and returns the index of the first free physical page.
+    // Returns -1 if no free page is found.
+    int getPage();
+    
+    // Frees the page at the given index.
+    void clearPage(int pageId);
 
-    public:
-        MemoryManager();
-        ~MemoryManager();
-
-        int AllocatePage();
-        int DeallocatePage(int which);
-        unsigned int GetFreePageCount();
-
-    private:
-        BitMap *bitmap;
-
+private:
+    BitMap *bitmap;  // Bitmap to track free/used physical pages.
+    Lock *lock;      // Lock to protect bitmap operations.
 };
 
-
-
-#endif // MEMORY_H
+#endif // MEMORY_MANAGER_H
