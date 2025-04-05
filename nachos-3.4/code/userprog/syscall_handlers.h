8e4fbd1ee5e22b80e95da6a2a374ddfd7bb5ea25
RR <67577204+rahuul2992000@users.noreply.github.com>
2025-04-05 19:51:21 -0400

    Created syscall_handlers.h

---
+++ nachos-3.4/code/userprog/syscall_handlers.h
@@ -0,0 +1,21 @@
+#ifndef SYSCALL_HANDLERS_H
+#define SYSCALL_HANDLERS_H
+
+#include "copyright.h"
+#include "thread.h"
+#include "synch.h"
+#include "addrspace.h"
+#include "pcb.h"
+#include "syscall.h"
+
+char* User2Kernel(int addr);
+
+// Function declarations for kernel-level syscall handlers
+SpaceId Fork(void (*func)());
+void SysExec();
+void SysExit();
+void SysYield();
+void SysJoin();
+void SysKill();
+
+#endif // SYSCALL_HANDLERS_H
