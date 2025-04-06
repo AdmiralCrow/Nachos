17ad4d25bde61bf570e4cb93c71e43c62100814f
AdmiralCrow <67215398+AdmiralCrow@users.noreply.github.com>
2025-04-06 10:07:27 -0400

    update pcb

--- nachos-3.4/code/userprog/pcb.cc
+++ nachos-3.4/code/userprog/pcb.cc
@@ -2,46 +2,38 @@
 #include <string.h>
 
 PCB::PCB(int id) {
-
     pid = id;
     parent = NULL;
     children = new List();
     thread = NULL;
     exitStatus = -9999;
+    nextFd = 0;
 
+    for (int i = 0; i < MAX_FILES; i++) {
+        fileTable[i] = NULL;
+    }
 }
-
-
 
 PCB::~PCB() {
-
     delete children;
-        for (std::map<int, OpenFile*>::iterator it = fileTable.begin(); it != fileTable.end(); ++it) {
-    delete it->second;
+    for (int i = 0; i < MAX_FILES; i++) {
+        if (fileTable[i] != NULL) {
+            delete fileTable[i];
+        }
+    }
 }
-}
-
-
 
 void PCB::AddChild(PCB* pcb) {
-
     children->Append(pcb);
-
-
 }
 
-
-int PCB::RemoveChild(PCB* pcb){
-
+int PCB::RemoveChild(PCB* pcb) {
     return children->RemoveItem(pcb);
-
 }
-
 
 bool PCB::HasExited() {
-    return exitStatus == -9999 ? false : true;
+    return exitStatus != -9999;
 }
-
 
 void decspn(int arg) {
     PCB* pcb = (PCB*)arg;
@@ -49,35 +41,34 @@ void decspn(int arg) {
     else pcb->parent = NULL;
 }
 
-
 void PCB::DeleteExitedChildrenSetParentNull() {
     children->Mapcar(decspn);
 }
+
 List* PCB::GetChildren() {
     return children;
 }
 
 int PCB::AllocateFileDescriptor(OpenFile* file) {
-    int fd = nextFd++;
-    fileTable[fd] = file;
-    return fd;
+    for (int i = 0; i < MAX_FILES; i++) {
+        if (fileTable[i] == NULL) {
+            fileTable[i] = file;
+            return i;
+        }
+    }
+    return -1; // No available file descriptor
 }
 
 OpenFile* PCB::GetFile(int fileDescriptor) {
-    std::map<int, OpenFile*>::iterator it = fileTable.find(fileDescriptor);
-
-    if (it != fileTable.end()) {
-        return it->second;
+    if (fileDescriptor >= 0 && fileDescriptor < MAX_FILES) {
+        return fileTable[fileDescriptor];
     }
     return NULL;
 }
 
 void PCB::ReleaseFileDescriptor(int fileDescriptor) {
-   std::map<int, OpenFile*>::iterator it = fileTable.find(fileDescriptor);
-
-    if (it != fileTable.end()) {
-        delete it->second; // Close the file
-        fileTable.erase(it);
+    if (fileDescriptor >= 0 && fileDescriptor < MAX_FILES && fileTable[fileDescriptor] != NULL) {
+        delete fileTable[fileDescriptor];
+        fileTable[fileDescriptor] = NULL;
     }
-
 }
