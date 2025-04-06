602d52c02714d9ded149b16de6aef376058696e5
AdmiralCrow <67215398+AdmiralCrow@users.noreply.github.com>
2025-04-06 17:30:10 -0400

    test

--- nachos-3.4/code/userprog/pcb.cc
+++ nachos-3.4/code/userprog/pcb.cc
@@ -1,6 +1,8 @@
 #include "pcb.h"
-#include "synch.h"    // bring in the full Condition definition
+#include "synch.h"
 #include "thread.h"
+#include "filesys.h"
+#include <map>
 
 PCB::PCB(Thread *thread)
 {
@@ -9,11 +11,18 @@ PCB::PCB(Thread *thread)
     processID = -1;      // Will be set by Process Manager.
     exitStatus = 0;      // Default exit status.
     joinCond = new Condition("joinCond");
+    nextFd = 0;          // Start file descriptor numbering from 0
 }
 
 PCB::~PCB()
 {
     delete joinCond;
+
+    // Close and delete all open files
+    for (std::map<int, OpenFile*>::iterator it = fileTable.begin(); it != fileTable.end(); ++it) {
+        delete it->second;
+    }
+    fileTable.clear();
 }
 
 int PCB::getID() const {
@@ -40,7 +49,32 @@ int PCB::getExitStatus() const {
     return exitStatus;
 }
 
-// Added this getter for processThread
 Thread* PCB::getThread() const {
     return processThread;
+}
+
+//----------------------------------------------------------------------
+// File descriptor management methods
+//----------------------------------------------------------------------
+
+int PCB::AllocateFileDescriptor(OpenFile* file) {
+    int fd = nextFd++;
+    fileTable[fd] = file;
+    return fd;
+}
+
+OpenFile* PCB::GetFile(int fileDescriptor) {
+    std::map<int, OpenFile*>::iterator it = fileTable.find(fileDescriptor);
+    if (it != fileTable.end()) {
+        return it->second;
+    }
+    return NULL;
+}
+
+void PCB::ReleaseFileDescriptor(int fileDescriptor) {
+    std::map<int, OpenFile*>::iterator it = fileTable.find(fileDescriptor);
+    if (it != fileTable.end()) {
+        delete it->second; // Close the file
+        fileTable.erase(it);
+    }
 }
