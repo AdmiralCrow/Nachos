1bf6acbd19bde2cac7acd531f8243243d5ebb6bb
AdmiralCrow <67215398+AdmiralCrow@users.noreply.github.com>
2025-04-06 00:03:46 -0400

    test

--- nachos-3.4/code/test/kill.c
+++ nachos-3.4/code/test/kill.c
@@ -1,16 +1,17 @@
 #include "syscall.h"
 
-void infinity(){
-	int i = 0;
-	for (; ;) Yield();
-}
-
-int main()
-{
-	int ret;
-	int id = Fork(infinity);
-	Yield();
-	ret = Kill(id);
-	Exit(ret);
-        return ret;
+int main() {
+    int pid = Fork();
+    if (pid == 0) {
+        // This is the child
+        while (1) {
+            Yield();  // Simulate doing something forever
+        }
+    } else {
+        // Parent
+        Yield();
+        Kill(pid);
+        Exit(0);
+    }
+    return 0; // not reached
 }
