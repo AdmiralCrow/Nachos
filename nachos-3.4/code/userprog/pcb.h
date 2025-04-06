bac81639fab7d3b69391764474518c796e64669c
AdmiralCrow <67215398+AdmiralCrow@users.noreply.github.com>
2025-04-06 17:45:59 -0400

    test

--- nachos-3.4/code/userprog/pcb.h
+++ nachos-3.4/code/userprog/pcb.h
@@ -1,35 +1,39 @@
 #ifndef PCB_H
 #define PCB_H
 
-#include "thread.h"   // for Thread*
+#include "list.h"
+#include "pcbmanager.h"
+#include "openfile.h"
+#include <map>
 
-class Condition;
 class Thread;
+class PCBManager;
+class Condition;
+class Lock;
+extern PCBManager* pcbManager;
 
 class PCB {
-public:
-    PCB(Thread *thread);  
-    ~PCB();
 
-    int getID() const;
-    void setID(int id);
+    public:
+        PCB(int id);
+        ~PCB();
+        int pid;
+        PCB* parent;
+        Thread* thread;
+        int exitStatus;
 
-    void setParent(PCB *parent);
-    PCB* getParent() const;
+        void AddChild(PCB* pcb);
+        int RemoveChild(PCB* pcb);
+        bool HasExited();
+        void DeleteExitedChildrenSetParentNull();
+        List* GetChildren();
 
-    void setExitStatus(int status);
-    int getExitStatus() const;
 
-    Thread* getThread() const;  // Getter for processThread
+    private:
+        List* children;
+        std::map<int, OpenFile*> fileTable; // Map for file descriptors
+        int nextFd; // Counter for the next file descriptor to allocate
 
-    // Condition variable for join (pointer only)
-    Condition *joinCond;
-
-private:
-    int processID;
-    Thread *processThread;
-    PCB *parentPCB;
-    int exitStatus;
 };
 
 #endif // PCB_H
