92d5edda768246952b5d5f268dd55ff4d5ea4e14
AdmiralCrow <67215398+AdmiralCrow@users.noreply.github.com>
2025-04-05 23:32:23 -0400

    update

--- nachos-3.4/code/threads/system.cc
+++ nachos-3.4/code/threads/system.cc
@@ -28,7 +28,11 @@ SynchDisk   *synchDisk;
 #endif
 
 #ifdef USER_PROGRAM	// requires either FILESYS or FILESYS_STUB
+#define MAX_PROCESSES 1024
 Machine *machine;	// user program memory and registers
+MemoryManager* mm;
+Lock* mmLock;
+PCBManager* pcbManager;
 #endif
 
 #ifdef NETWORK
